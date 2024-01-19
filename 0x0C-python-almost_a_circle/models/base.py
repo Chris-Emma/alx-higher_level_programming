0ac5935154c0acf793be5bfbaa17d25fb1b28657
Chris-Emma <chrisemmaofficial@gmail.com>
2024-01-19 05:20:46 -0800

    circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""Defines a base model class."""
 
 class Base:
-     """
-     Represent the base model
-     """
-     __nb_objects = 0
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
 
-     def __init__(self, id=None):
-         """Initializes new Base"""
+    """
 
-         if id is not None:
-             self.id = id
-         else:
-             Base.__nb_objects += 1
-             self.id = Base.__nb_objects
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
