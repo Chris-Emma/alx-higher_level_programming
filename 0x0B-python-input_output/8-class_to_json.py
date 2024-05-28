1b18352e3f664fdbea9f184abe814a2e92cdd3de
Chris-Emma <chrisemmaofficial@gmail.com>
2024-05-28 01:43:28 -0700

    input_output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
+
+""" Module to return the dictionary description with simple data structure
 """
-Script
-Adds all arguments to a Python list, and then saves them to a file
-"""
 
-from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
-
-try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
-
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+def class_to_json(obj):
+    """
+    Class to json object
+    Return:
+        The dict for JSON serialization of an object
+    """
+    return obj.__dict__
