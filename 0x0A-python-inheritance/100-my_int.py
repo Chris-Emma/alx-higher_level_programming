4f8b4e1a43d93e699034a0a160eb638dfac59f61
Chris-Emma <chrisemmaofficial@gmail.com>
2024-05-28 00:44:30 -0700

    inheritence

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Module MyInt
+Inherit from int
+"""
+
+
+class MyInt(int):
+    """
+    class utilizing int class that will
+    define a class that inverts == !=. operators
+    """
+
+    def __eq__(self, other):
+        """initializer for eq"""
+        if int.__ne__(self, other):
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        """intializer for greater than"""
+        if int.__eq__(self, other):
+            return True
+        else:
+            return False
